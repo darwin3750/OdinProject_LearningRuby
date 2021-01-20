a53599e4b07003fb27313cfd7b451f2e1a2870b7
darwin3750 <damedel@mymail.mapua.edu.ph>
2021-01-20 18:19:12 +0800

    add finished stock_picker

---
+++ stock_picker.rb
@@ -0,0 +1,18 @@
+def stock_picker (array)
+  result = {:buy_price => 9999, :buy_index => -1, :sell_price => 0, :sell_index => 9999}
+  profit = -1
+  array.each_with_index{ |value, index|
+    if((result[:sell_price] - value > profit) && index < result[:sell_index])
+      result[:buy_price] = value
+      result[:buy_index] = index
+    end
+    if((value - result[:buy_price] > profit) && index > result[:buy_index])
+      result[:sell_price] = value
+      result[:sell_index] = index
+    end
+    profit = result[:sell_price] - result[:buy_price]
+  }
+  puts result
+end
+
+stock_picker([17,3,6,9,15,8,6,1,10])
